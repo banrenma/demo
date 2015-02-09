c5c8c355a1e8c38129ee131133744648b4a29aea
nieming <501182555@qq.com>
2015-02-10 00:22:37 +0800

    update

--- Classes/aStar.cpp
+++ Classes/aStar.cpp
@@ -47,3 +47,96 @@ void MapData::resetGridVisit()
 	}
 }
 
+
+std::vector<Vec2>  aStar::BeginSearchPath( Vec2 starPosition,Vec2 endPosition )
+{
+	std::vector<Vec2> path;
+	m_vecOpen.clear();
+	//第0个位置 不占用
+	m_vecOpen.push_back(Vec2::ZERO);
+	if(starPosition == endPosition || isPtPonderable(starPosition) || isPtPonderable(endPosition) )
+	{
+		return path;
+	}
+	
+	m_vecOpen.push_back(starPosition);
+
+	aStarNode * node = new aStarNode(NULL,0,getH(starPosition,endPosition),starPosition);
+	m_pMapData->setGridaStarNode(starPosition,node);
+
+	 while(m_vecOpen.size() > 1)
+	 {
+		 aStarNode * node1 = m_pMapData->getGridaStarNode(*(m_vecOpen.begin()+1));
+		 if(node1 == NULL)
+		 {
+			 log("error node1 is NULL");
+			 break;
+		 }
+			 
+		 // 删除f 最小的节点
+		 for(int i=0; i < 8;i++)
+		 {
+			 int count = 0;
+			 Vec2 pt(node->getPt().x + i%3 -1,node->getPt().x + i/3 -1);
+			 //找到终点
+			 if(pt == endPosition)
+			 {
+				 goto GetPATH;
+			 }
+			 // 判断条件 该点有效    不是障碍物
+			 if(isPtPonderable(pt)  &&  m_pMapData->getGridWeight(pt) <= 200)
+			 {
+				 //把这点存在 关闭列表
+				  aStarNode * node2 = m_pMapData->getGridaStarNode(pt);
+				if(node2==NULL )
+				{
+					//添加一个字节到open列表
+					//替换掉 顶部元素
+					if(count == 0)
+					{
+						//替换掉 顶部元素 从新排列
+
+					}
+					else
+					{
+						//添加到末尾 从新排列
+					}
+
+				}
+				else if(node2->isVisit() == true)
+				{
+					//比较 g值  从新生成堆
+					if(count == 0)
+					{
+						//直接和顶部元素
+					}
+					else
+					{
+
+					}
+				}
+				//
+
+			 }
+		 }
+		
+	 }
+GetPATH:
+
+	 //没找到
+	 return path;
+
+
+}
+
+
+
+bool aStar::isPtPonderable( Vec2 pt )
+{
+	return pt.x >=0 && pt.x < m_pMapData->getMaxx() && pt.y >=0 && pt.y <m_pMapData->getMaxy();
+}
+
+void aStar::pushChildinOpen( aStarNode * node )
+{
+
+}
--- Classes/aStar.h
+++ Classes/aStar.h
@@ -9,9 +9,9 @@ class aStarNode
 {
 public:
 	aStarNode(){}
-	aStarNode(aStarNode * pParent,int f,int g,int h,Vec2 pt){
+	aStarNode(aStarNode * pParent,int g,int h,Vec2 pt){
 		m_pParent = pParent;
-		m_if = f;
+		m_if = g+h;
 		m_ig = g;
 		m_ih = h;
 		m_pt = pt;
@@ -21,6 +21,7 @@ public:
 	int getG(){return m_ig;}
 	int getH(){return m_ih;}
 	bool isVisit(){return m_isVisit;}
+	Vec2 getPt(){return m_pt;};
 	aStarNode(aStarNode & data)
 	{
 		m_pParent = data.m_pParent;
@@ -74,39 +75,39 @@ public:
 	int m_maxx;
 	int m_maxy;
 	std::map<Vec2,MapGridData> m_data;
-	void init();
-
 	void setGridWeight( Vec2 pt ,int weight);
 	int getGridWeight(Vec2 pt);
 	void resetGridVisit();
 	void setGridaStarNode( Vec2 pt,aStarNode* paStarNode );
 	aStarNode* getGridaStarNode (Vec2 pt);
-	void 
+	int getMaxx(){return m_maxx;};
+	int getMaxy(){return m_maxy;};
 };
 
 
 class aStar
 {
 public:
-	enum searchMode
+	enum class searchMode
 	{
 		eightOrientation,
 		fourOrientation
 	};
+	aStar(MapData * pMap);
+	std::vector<Vec2> BeginSearchPath(Vec2 starPosition,Vec2 endPosition);
+protected:
 	//父节点 移动 到子节点 的代价
 	virtual int getG(Vec2 parentPt,Vec2 pt);
 	//子节点 移动到 目的地 的估算代价
 	virtual int getH(Vec2 pt,Vec2 targetPt);
-
-
-
-	aStar(MapData * pMap);
-
-	bool BeginSearchPath(Vec2 starPosition,Vec2 endPosition);
-
+	//从8个方向把子child 放到开放列表
+	void pushChildinOpen(aStarNode * node);
+	
+	//是否是有效的
+	bool isPtPonderable(Vec2 pt);
 	//开放列表 二叉堆
-	std::vector<aStarNode *> m_vecOpen;
-
+	std::vector<Vec2> m_vecOpen;
+	MapData *m_pMapData;
 
 };
 #endif // __ASTART__
